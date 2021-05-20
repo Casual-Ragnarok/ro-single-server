496821f628df6e53f62f4dab545c8fd4cfc9c6ff
EXP <289065406@qq.com>
2021-05-20 23:45:46 +0800

    添加 metabase

--- ROEmulator/docker/apache2/www/data/template/1_1_common_header_forum_index.tpl.php
+++ ROEmulator/docker/apache2/www/data/template/1_1_common_header_forum_index.tpl.php
@@ -1,8 +1,8 @@
 <?php if(!defined('IN_DISCUZ')) exit('Access Denied'); 
 0
-|| checktplrefresh('./template/default/common/header.htm', './template/default/common/header_common.htm', 1426590650, '1', './data/template/1_1_common_header_forum_index.tpl.php', './template/default', 'common/header_forum_index')
-|| checktplrefresh('./template/default/common/header.htm', './template/default/common/header_qmenu.htm', 1426590650, '1', './data/template/1_1_common_header_forum_index.tpl.php', './template/default', 'common/header_forum_index')
-|| checktplrefresh('./template/default/common/header.htm', './template/default/common/pubsearchform.htm', 1426590650, '1', './data/template/1_1_common_header_forum_index.tpl.php', './template/default', 'common/header_forum_index')
+|| checktplrefresh('./template/default/common/header.htm', './template/default/common/header_common.htm', 1621525375, '1', './data/template/1_1_common_header_forum_index.tpl.php', './template/default', 'common/header_forum_index')
+|| checktplrefresh('./template/default/common/header.htm', './template/default/common/header_qmenu.htm', 1621525375, '1', './data/template/1_1_common_header_forum_index.tpl.php', './template/default', 'common/header_forum_index')
+|| checktplrefresh('./template/default/common/header.htm', './template/default/common/pubsearchform.htm', 1621525375, '1', './data/template/1_1_common_header_forum_index.tpl.php', './template/default', 'common/header_forum_index')
 ;?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
